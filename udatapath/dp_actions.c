6fb8891230e1b4879bf3f538d7cadd077ed4f854
Khaled Diab <khaledmdiab@hotmail.com>
2017-07-11 17:17:33 -0700

    unfinished

--- udatapath/dp_actions.c
+++ udatapath/dp_actions.c
@@ -47,8 +47,14 @@
 #include "util.h"
 #include "oflib/oxm-match.h"
 #include "hash.h"
+#include "../lib/packets.h"
+#include "../lib/list.h"
+#include "../oflib/ofl-structs.h"
 
 #define LOG_MODULE VLM_dp_acts
+
+#define SRMCAST_CODE_SHIFT 18
+#define SRMCAST_CONTENT_MASK 0x3ffff
 
 static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(60, 60);
 
@@ -743,6 +749,78 @@ push_mpls(struct packet *pkt, struct ofl_action_push *act) {
 }
 
 /* Executes pop mpls action. */
+//static void
+//_pop_mpls(struct packet *pkt, struct ofl_action_pop_mpls *act) {
+//    packet_handle_std_validate(pkt->handle_std);
+//    if (pkt->handle_std->proto->eth != NULL && pkt->handle_std->proto->mpls != NULL) {
+//        struct eth_header *eth = pkt->handle_std->proto->eth;
+//        struct snap_header *snap = pkt->handle_std->proto->eth_snap;
+//        struct vlan_header *vlan_last = pkt->handle_std->proto->vlan_last;
+//        struct mpls_header *mpls = pkt->handle_std->proto->mpls;
+//        size_t move_size;
+//
+//        if (vlan_last != NULL) {
+//            vlan_last->vlan_next_type = htons(act->ethertype);
+//        } else if (snap != NULL) {
+//            snap->snap_type = htons(act->ethertype);
+//        } else {
+//            eth->eth_type = htons(act->ethertype);
+//        }
+//
+//        move_size = (uint8_t *)mpls - (uint8_t *)eth;
+//        pkt->buffer->data = (uint8_t *)pkt->buffer->data + MPLS_HEADER_LEN;
+//        pkt->buffer->size -= MPLS_HEADER_LEN;
+//        memmove(pkt->buffer->data, eth, move_size);
+//
+//        if (snap != NULL) {
+//            struct eth_header *new_eth = (struct eth_header *)(pkt->buffer->data);
+//            new_eth->eth_type = htons(ntohs(new_eth->eth_type) + MPLS_HEADER_LEN);
+//        }
+//
+//        //TODO Zoltan: revalidating might not be necessary at all cases
+//        pkt->handle_std->valid = false;
+//    } else {
+//        VLOG_WARN_RL(LOG_MODULE, &rl, "Trying to execute POP_MPLS action on packet with no eth/mpls.");
+//    }
+//}
+
+
+static void
+clean_mpls_pkt(struct packet *pkt) {
+    packet_handle_std_validate(pkt->handle_std);
+    if (pkt->handle_std->proto->eth != NULL && pkt->handle_std->proto->mpls != NULL) {
+        struct eth_header *eth = pkt->handle_std->proto->eth;
+        struct mpls_header *mpls = pkt->handle_std->proto->mpls;
+        size_t move_size;
+        uint32_t label_count;
+
+        label_count = 0;
+        while (eth != NULL && mpls != NULL) {
+            VLOG_WARN(LOG_MODULE, "BEFORE: eth = %p\n", eth);
+            VLOG_WARN(LOG_MODULE, "BEFORE: mpls = %p\n", mpls);
+            VLOG_WARN(LOG_MODULE, "BEFORE: data = %p\n", pkt->buffer->data);
+            label_count += 1;
+            eth->eth_type = htons(ETH_TYPE_IP);
+            move_size = (uint8_t *)mpls - (uint8_t *)eth;
+            pkt->buffer->data = (uint8_t *)pkt->buffer->data + MPLS_HEADER_LEN;
+            pkt->buffer->size -= MPLS_HEADER_LEN;
+            memmove(pkt->buffer->data, eth, move_size);
+            VLOG_WARN(LOG_MODULE, "move_size = %zu\n", move_size);
+            pkt->handle_std = packet_handle_std_create(pkt);
+//            packet_handle_std_validate(pkt->handle_std);
+            eth = pkt->handle_std->proto->eth;
+            mpls = pkt->handle_std->proto->mpls;
+            VLOG_WARN(LOG_MODULE, "AFTER: eth = %p\n", eth);
+            VLOG_WARN(LOG_MODULE, "AFTER: mpls = %p\n", mpls);
+            VLOG_WARN(LOG_MODULE, "AFTER: data = %p\n", pkt->buffer->data);
+        }
+        VLOG_WARN(LOG_MODULE, "label_count = %d\n", label_count);
+    } else {
+        VLOG_WARN_RL(LOG_MODULE, &rl, "Trying to execute Clean MPLS on packet with no eth/mpls.");
+    }
+}
+
+// SR-MCAST implementation
 static void
 pop_mpls(struct packet *pkt, struct ofl_action_pop_mpls *act) {
     packet_handle_std_validate(pkt->handle_std);
@@ -751,7 +829,18 @@ pop_mpls(struct packet *pkt, struct ofl_action_pop_mpls *act) {
         struct snap_header *snap = pkt->handle_std->proto->eth_snap;
         struct vlan_header *vlan_last = pkt->handle_std->proto->vlan_last;
         struct mpls_header *mpls = pkt->handle_std->proto->mpls;
+
         size_t move_size;
+        struct datapath *dp;
+        struct packet *pkt_clone;
+        uint32_t mpls_fields;
+        uint32_t mpls_label;
+        uint32_t srmcast_code;
+        uint32_t srmcast_content;
+
+        uint32_t port_id;
+        uint32_t port_shift;
+        struct sw_port *p;
 
         if (vlan_last != NULL) {
             vlan_last->vlan_next_type = htons(act->ethertype);
@@ -760,13 +849,50 @@ pop_mpls(struct packet *pkt, struct ofl_action_pop_mpls *act) {
         } else {
             eth->eth_type = htons(act->ethertype);
         }
+        dp = pkt->dp;
+        mpls_fields = ntohl(mpls->fields);
+        mpls_label = mpls_fields >> MPLS_LABEL_SHIFT;
+        srmcast_code = mpls_label >> SRMCAST_CODE_SHIFT;
+        srmcast_content = mpls_label & SRMCAST_CONTENT_MASK;
+
+        if (srmcast_code == 0) {
+            // multicast label
+            pkt_clone = packet_clone(pkt);
+            VLOG_WARN(LOG_MODULE, "Before clean %s\n", packet_to_string(pkt_clone));
+            clean_mpls_pkt(pkt_clone);
+            VLOG_WARN(LOG_MODULE, "After clean %s\n", packet_to_string(pkt_clone));
+            VLOG_WARN(LOG_MODULE, "Multicast label: %d\n", srmcast_content);
+            LIST_FOR_EACH (p, struct sw_port, node, &dp->port_list) {
+                if (IS_HW_PORT(p) || p->conf->port_no <= 0) {
+                    continue;
+                }
+                port_id = 1;
+                port_shift = p->conf->port_no - 1;
+                if ((port_id << port_shift) & srmcast_content) {
+//                    VLOG_WARN(LOG_MODULE, "Port number: %d, name: %s\n", p->conf->port_no, p->conf->name);
+                }
+            }
+
+        } else if (srmcast_code == 1) {
+            // offset label
+            VLOG_WARN(LOG_MODULE, "Offset label: %d\n", srmcast_content);
+
+        } else if (srmcast_code == 2) {
+            // node label
+            VLOG_WARN(LOG_MODULE, "Node label: %d\n", srmcast_content);
+
+        } else if (srmcast_code == 3) {
+            // adjacent label
+            VLOG_WARN(LOG_MODULE, "Adjacent label: %d\n", srmcast_content);
+        }
 
         move_size = (uint8_t *)mpls - (uint8_t *)eth;
-
         pkt->buffer->data = (uint8_t *)pkt->buffer->data + MPLS_HEADER_LEN;
         pkt->buffer->size -= MPLS_HEADER_LEN;
 
         memmove(pkt->buffer->data, eth, move_size);
+
+        VLOG_WARN(LOG_MODULE, "Move size: %zu\n", move_size);
 
         if (snap != NULL) {
             struct eth_header *new_eth = (struct eth_header *)(pkt->buffer->data);
